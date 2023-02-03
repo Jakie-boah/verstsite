b03e4871303b5f4c4f1348f79792b48654c0c37e
NAME <VanoGalen@yandex.ru>
2023-02-03 23:31:45 +0300

    2023-02-03 23:31:45

--- verstsite/urls.py
+++ verstsite/urls.py
@@ -22,16 +22,16 @@ from .views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('/register', register, name='register'),
-    path('/login', log_in, name='login'),
-    path('/check', check, name='check'),
-    path('/reviews', reviews, name='reviews'),
-    path('/vacancy', vacancy, name='vacancy'),
-    path('/contacts', contacts, name='contacts'),
-    path('/sales', sales, name='sales'),
-    path('/buy', buy, name='buy'),
-    path('/choose', choose, name='choose'),
-    path('/choose1', choose1, name='choose1'),
-    path('/end', end, name='end'),
-    path('/add', add, name='add')
+    path('register', register, name='register'),
+    path('login', log_in, name='login'),
+    path('check', check, name='check'),
+    path('reviews', reviews, name='reviews'),
+    path('vacancy', vacancy, name='vacancy'),
+    path('contacts', contacts, name='contacts'),
+    path('sales', sales, name='sales'),
+    path('buy', buy, name='buy'),
+    path('choose', choose, name='choose'),
+    path('choose1', choose1, name='choose1'),
+    path('end', end, name='end'),
+    path('add', add, name='add')
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
